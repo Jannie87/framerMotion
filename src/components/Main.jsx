7dac9f6e6dfb76917c555da158d9186bcf3df228
Jannie87 <jannie.backman@gmail.com>
2022-02-12 10:26:32 +0100

    removed card css and jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,21 @@
 import { Component } from "react";
 import "./main.css";
-import Card from "./Card";
 import { motion } from "framer-motion";
 
 class Main extends Component {
   constructor(props) {
     super(props);
-    //   this.state = { cars: ["Legacy", "Impreza", "Outback", "Forester"] };
-    //this state endast i constructor
   }
 
   render() {
     return (
       <main className="container">
         <div className="round-boxes">
-          <div className="one" />
-          <div className="two" />
-          <div className="three" />
-          <div className="four" />
+          <motion.div drag className="one" />
+          <motion.div drag className="two" />
+          <motion.div drag className="three" />
+          <motion.div drag className="four" />
         </div>
-        {/* <div className="column">
-          {this.props.isListVisable && (
-            <ul className="card-container">
-              {this.state.cars.map((car) => (
-                <Card key={car}>{car}</Card>
-              ))}
-            </ul>
-          )}
-        </div> */}
       </main>
     );
   }
