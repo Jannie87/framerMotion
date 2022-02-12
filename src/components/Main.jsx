5509d4a9b82ef535786f380d4b2ed1f609166f57
Jannie87 <jannie.backman@gmail.com>
2022-02-12 11:41:47 +0100

    added rotation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,15 @@ class Main extends Component {
       <main className="container">
         <div className="round-boxes">
           <motion.div drag className="one" />
-          <motion.div drag className="two" />
+          <motion.div
+            drag
+            whileTap={{
+              scale: [1, 2, 2, 1, 1],
+              rotate: [0, 0, 270, 270, 0],
+              borderRadius: ["50%", "50%", "40%", "50%", "50%"],
+            }}
+            className="two"
+          />
           <motion.div drag className="three" />
           <motion.div drag className="four" />
         </div>
